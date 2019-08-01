b0d7161405c80bb7588cd4174c66c0493bd813b3
Bella Loia <bella@loia.com>
2019-08-01 16:13:44 -0400

    Inital Commit

--- womED_project/ViewController.swift
+++ womED_project/ViewController.swift
@@ -2,7 +2,7 @@
 //  ViewController.swift
 //  womED_project
 //
-//  Created by Apple on 7/31/19.
+//  Created by Apple on 8/1/19.
 //  Copyright © 2019 KWK. All rights reserved.
 //
 
@@ -10,11 +10,27 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    @IBAction func btnAction(_ sender: UIButton) {
+        guard let numberString = sender.titleLabel?.text, let url = URL(string: "telprompt://\(numberString)")else{
+            return
+        }
+        UIApplication.shared.open(url)
+    }
+    override var prefersStatusBarHidden: Bool{
+        return true
     }
     
-   
-}
+    override func viewDidLoad() {
+        super.viewDidLoad()
 
+      
+}
+  
+    //@IBAction func onCallPress(_ sender: UIButton) {
+      //  let url:NSURL = URL(string:"TEL://\(self.phoneNumber.text)")! as NSURL
+        //UIApplication.shared.open(URL,options: ,completionHandler)
+    }
+
+ 
+ 
+ 
